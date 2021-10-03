8ce00ce03b372e54dc68dd9bc4a72f38159930c9
Sergi Mas <sergi.mas@gmail.com>
2021-10-03 17:06:38 +0200

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Adipisicing sint velit excepteur incididunt culpa dolore eu reprehenderit magna. Labore consectetur et eiusmod sunt sint eu sunt et sunt magna mollit esse. Exercitation sint non sunt fugiat cupidatat pariatur consectetur. Pariatur qui enim adipisicing eu magna occaecat commodo. Sit elit est sint aute laborum aliqua reprehenderit irure ullamco laborum aute consequat sint. Qui cupidatat duis officia dolor consequat nostrud aute do veniam qui minim dolor enim. Pariatur exercitation aliqua proident sunt incididunt tempor sint.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Id dolore velit aliquip quis deserunt nisi dolore dolor do. Irure dolor dolore deserunt anim laboris irure anim pariatur commodo Lorem labore eu dolore. Consequat consectetur Lorem sunt Lorem. Irure incididunt pariatur velit enim sunt laborum nisi.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Amet laboris cillum dolore ea ea esse ullamco. Officia cupidatat non et nisi dolore do laborum adipisicing proident dolor sit. Elit excepteur aliquip irure ex laboris qui nostrud aliquip nulla. Lorem elit officia aliqua deserunt sunt eiusmod esse sunt.',
+            picture: null,
+        },
+    ],
+})
